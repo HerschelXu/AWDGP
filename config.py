db196c752940708a2362e6731a091c6eb144870b
WWW <145915296+WWH987@users.noreply.github.com>
2024-05-19 01:38:37 +0800

    Update config.py
    Reopen the SMTP server

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 MAIL_USERNAME = 'wangweihua1997@qq.com'
-MAIL_PASSWORD = 'rocelfnazapubdgb'
+MAIL_PASSWORD = 'jnaymgrobtapbbhg'
 MAIL_DEFAULT_SENDER = "wangweihua1997@qq.com"
 
